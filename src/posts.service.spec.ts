18f3624612994f74e35fd5202c245b30f6014ace
Alex2234 <shyrik223@mail.ru>
2024-07-14 15:19:53 +0300

    one

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,22 +1,46 @@
-import { Post, PostsService } from './posts.service';
+import { Post, PostsService } from './posts.service'
 
 describe('PostsService', () => {
-  let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
-  };
+	let postsService: PostsService
+	const post: Omit<Post, 'id' | 'date'> = {
+		text: 'Mocked post',
+	}
 
-  beforeEach(async () => {
-    postsService = new PostsService();
+	beforeEach(async () => {
+		postsService = new PostsService()
 
-    postsService.create({ text: 'Some pre-existing post' });
-  });
+		postsService.create({ text: 'Some pre-existing post' })
+	})
 
-  it('should add a new post', () => {
-    // реализуйте тест-кейс
-  });
+	it('should add a new post', () => {
+		// Количество постов до добавления нового поста
+		const initialPosts = postsService['posts']
+		const initialPostCount = initialPosts.length
 
-  it('should find a post', () => {
-    // реализуйте тест-кейс
-  });
-});
+		// Добавление нового поста
+		const newPost = postsService.create(post)
+
+		// Проверка количества постов после добавления нового поста
+		const allPosts = postsService['posts']
+		expect(allPosts.length).toBe(initialPostCount + 1)
+		expect(allPosts).toContainEqual(newPost)
+
+		// Проверка атрибутов нового поста
+		expect(newPost.text).toBe(post.text)
+		expect(newPost.id).toBeDefined()
+		expect(newPost.date).toBeDefined()
+	})
+
+	it('should find a post', () => {
+		// Создание нового поста
+		const newPost = postsService.create(post)
+
+		// Поиск поста по id
+		const foundPost = postsService.find(newPost.id)
+
+		// Проверка найденного поста
+		expect(foundPost).toBeDefined()
+		expect(foundPost?.id).toBe(newPost.id)
+		expect(foundPost?.text).toBe(newPost.text)
+	})
+})
